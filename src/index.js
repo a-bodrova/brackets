61499eadf19625631c34060badd9e81c97a15255
a-bodrova <soulwent94@gmail.com>
2021-08-29 16:51:30 +0400

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  let stack = [];
+  const OPEN_BRACKETS = [];
+  const CLOSE_BRACKETS = [];
+  const BRACKETS_PAIRS = {};
+  
+  for (let item of config) {
+    OPEN_BRACKETS.push(item[0]);
+    CLOSE_BRACKETS.push(item[1]);
+  }
+
+  for (let i = 0; i < config.length; i++) {
+    BRACKETS_PAIRS[CLOSE_BRACKETS[i]] = OPEN_BRACKETS[i];
+  }
+  
+  for (let i = 0; i < str.length; i++) {
+  let currentSymbol = str[i];
+  
+  if (OPEN_BRACKETS.includes(currentSymbol) && !CLOSE_BRACKETS.includes(currentSymbol)) {
+    stack.push(currentSymbol);
+  } else {
+    
+    let topElement = stack[stack.length - 1];
+
+    if (BRACKETS_PAIRS[currentSymbol] === topElement) {
+      stack.pop();
+    } else {
+      stack.push(currentSymbol);
+    }
+  }
+}
+
+return stack.length === 0;
 }
